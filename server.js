2ca85ebc6f6706c27317ec7fb9e2478c96b98311
dereckSOC <dereckzorca.18@ichat.sp.edu.sg>
2021-07-09 12:53:32 +0800

    7

--- server.js
+++
@@ -1,8 +0,0 @@
-var app=require('./app.js');
-var port= process.env.PORT || 3000;
-
-var server=app.listen(port,function(){
-    console.log("=====================================================")
-    console.log("Server launched. App hosted at localhost: "+ port);
-    console.log("=====================================================")
-});
