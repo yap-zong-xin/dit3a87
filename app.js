b19c35a9a841baf2501a86cb006d605b4452ef9a
avke03 <avk03.19@ichat.sp.edu.sg>
2021-07-11 12:38:43 +0800

    chat client test 5

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(flash());
 
-app.use(express.static('/client/build'));
+app.use(express.static('/chat/client/build'));
 
 //use & run express session
 app.use(require("express-session")({
