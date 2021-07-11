235ea2264918ad76554f6a9662554c617d118a41
avke03 <avk03.19@ichat.sp.edu.sg>
2021-07-12 01:28:33 +0800

    chat app

--- chat/client/src/components/chatOnline/ChatOnline.jsx
+++ chat/client/src/components/chatOnline/ChatOnline.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import {Button, Jumbotron, Container} from "reactstrap"
 import "./chatOnline.css";
 
