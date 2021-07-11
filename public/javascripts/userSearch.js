dcb224365d5f53d10b11a29f8abebb461c4691a3
dereckSOC <dereckzorca.18@ichat.sp.edu.sg>
2021-07-11 18:11:40 +0800

    lazy load (find agent) 5

--- public/javascripts/userSearch.js
+++ public/javascripts/userSearch.js
@@ -1,8 +1,33 @@
-function loadData(rs) {
+function loadData(rs,loadObj) {
+    console.log(loadObj)
+    //loadObj
+    var totalRem = loadObj.totalRem;
+    var sectionRem = loadObj.sectionRem;
+    var remainUser = loadObj.remainUser;
+    var startIndex = loadObj.startIndex;
+    var endIndex;
+
+    //loads all data
     var resultSet = JSON.parse(rs);
     var insertDiv = document.getElementById("resultDiv");
+    var i;
 
-    for (var i = 0; i < resultSet.length; i++) {
+    if (sectionRem == 0) {
+        endIndex = startIndex + remainUser;
+        //remaining minus 5 (as it has already been loaded)
+        loadObj.totalRem = 0;
+        //section minus 1 (as it has already been loaded)
+        loadObj.sectionRem = 0;
+        //remainUser remains the same
+        loadObj.remainUser = 0;
+        //end index becomes start index
+        loadObj.startIndex = 0;
+    } else {
+        endIndex = startIndex + 5;
+    }
+
+
+    for (i = startIndex; i < endIndex; i++) {
         var html;
         if (resultSet[i].rating === 0) {
             html = `<div class="col-md-12">
@@ -127,4 +152,52 @@ function loadData(rs) {
         }
         insertDiv.insertAdjacentHTML("beforebegin", html);
     }
+
+    //update object
+    // var totalRem = loadObj.totalRem;
+    // var sectionRem = loadObj.sectionRem;
+    // var remainUser = loadObj.remainUser;
+    // var startIndex = loadObj.startIndex;
+    //if section is not done
+    if (sectionRem != 0) {
+        //remaining minus 5 (as it has already been loaded)
+        loadObj.totalRem = loadObj.totalRem - 5;
+        //section minus 1 (as it has already been loaded)
+        loadObj.sectionRem = loadObj.sectionRem - 1;
+        //remainUser remains the same
+        //loadObj[2]
+        //end index becomes start index
+        loadObj.startIndex = endIndex;
+    } else {
+        //remaining minus 5 (as it has already been loaded)
+        loadObj.totalRem = loadObj.remainUser;
+        //section minus 1 (as it has already been loaded)
+        loadObj.sectionRem = 0;
+        //remainUser remains the same
+        //loadObj[2]
+        //end index becomes start index
+        loadObj.startIndex = endIndex;
+    }
+
+    return loadObj;
+}
+
+function loadFirstFive(rs) {
+    var resultSet = JSON.parse(rs);
+    var rsLength = resultSet.length;
+    var loadObj;
+
+    //divide into different sections
+    var section = Math.floor(parseInt(rsLength,10) / 5); //number of sections (1 section, 5 users)
+    var remSect = parseInt(rsLength,10) % 5; //number of users remaining
+
+    var loadObj = {
+        "totalRem" : rsLength,
+        "sectionRem" : section,
+        "remainUser" : remSect,
+        "startIndex" : 0,
+    }
+
+    loadObj = loadData(rs,loadObj);
+    return loadObj;
 }
