87a07062bf8746a9a521d80ff90413876bc64cfb
dereckSOC <dereckzorca.18@ichat.sp.edu.sg>
2021-07-15 22:13:41 +0800

    find agents 2 (responsive except for banner)

--- public/javascripts/userSearch.js
+++ public/javascripts/userSearch.js
@@ -32,8 +32,8 @@ function loadData(rs,loadObj) {
         if (resultSet[i].rating === 0) {
             html = `<div class="col-md-12" data-aos="fade-up" data-aos-once="true">
                         <div class="col-md-12 user-result" onClick="window.location='/user/` + resultSet[i]._id + `'">
-                            <div class="row col-md-9 m-0" style="padding: 10px;">
-                                <img src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
+                            <div class="row col-md-9 col-6 m-0 user-detail" style="padding: 10px;">
+                                <img class="img-fluid" src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
                                 <div class="caption-result">
                                     <h4>`+ resultSet[i].firstName + ` ` + resultSet[i].lastName + `</h4>
                                     <h6>CEA: ` + resultSet[i].cea + `</h6>
@@ -44,19 +44,19 @@ function loadData(rs,loadObj) {
                                     <em style="color:rgb(163, 163, 163);">No reviews found.</em>
                                 </div>
                             </div>
-                            <div class="user-result-contact col-md-3 m-0">
+                            <div class="user-result-contact col-md-3 col-6 m-0">
                                 <h5>Agent Details</h5>
                                 <div class="contact-row">
                                     <i class="far fa-envelope"></i>
-                                    <p>: ` + resultSet[i].email + `</p>
+                                    <p>` + resultSet[i].email + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <i class="fas fa-phone-alt"></i>
-                                    <p>: ` + resultSet[i].phone + `</p>
+                                    <p>` + resultSet[i].phone + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <p><b style="font-weight: 600;">Member Since</b></p>
-                                    <p>: ` + dateMonthAsWord + `</p>
+                                    <p>` + dateMonthAsWord + `</p>
                                 </div>
                             </div>
                         </div>
@@ -80,8 +80,8 @@ function loadData(rs,loadObj) {
                 case 0:
                     html = `<div class="col-md-12" data-aos="fade-up" data-aos-once="true">
                             <div class="col-md-12 user-result" onClick="window.location='/user/` + resultSet[i]._id + `'">
-                                <div class="row col-md-9 m-0" style="padding: 10px;">
-                                <img src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
+                                <div class="row col-md-9 col-6 m-0 user-detail" style="padding: 10px;">
+                                <img class="img-fluid" src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
                                 <div class="caption-result">
                                     <h4>`+ resultSet[i].firstName + ` ` + resultSet[i].lastName + `</h4>
                                     <h6>CEA: ` + resultSet[i].cea + `</h6>
@@ -96,19 +96,19 @@ function loadData(rs,loadObj) {
                                     <span class="fa fa-star"></span>
                                 </div>
                             </div>
-                            <div class="user-result-contact col-md-3 m-0">
+                            <div class="user-result-contact col-md-3 col-6 m-0">
                                 <h5>Agent Details</h5>
                                 <div class="contact-row">
                                     <i class="far fa-envelope"></i>
-                                    <p>: ` + resultSet[i].email + `</p>
+                                    <p>` + resultSet[i].email + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <i class="fas fa-phone-alt"></i>
-                                    <p>: ` + resultSet[i].phone + `</p>
+                                    <p>` + resultSet[i].phone + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <p><b style="font-weight: 600;">Member Since</b></p>
-                                    <p>: ` + dateMonthAsWord + `</p>
+                                    <p>` + dateMonthAsWord + `</p>
                                 </div>
                             </div>
                             </div>
@@ -117,8 +117,8 @@ function loadData(rs,loadObj) {
                 case 1:
                     html = `<div class="col-md-12" data-aos="fade-up" data-aos-once="true">
                             <div class="col-md-12 user-result" onClick="window.location='/user/` + resultSet[i]._id + `'">
-                                <div class="row col-md-9 m-0" style="padding: 10px;">
-                                <img src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
+                                <div class="row col-md-9 col-6 m-0 user-detail" style="padding: 10px;">
+                                <img class="img-fluid" src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
                                 <div class="caption-result">
                                     <h4>`+ resultSet[i].firstName + ` ` + resultSet[i].lastName + `</h4>
                                     <h6>CEA: ` + resultSet[i].cea + `</h6>
@@ -133,19 +133,19 @@ function loadData(rs,loadObj) {
                                     <span class="fa fa-star"></span>
                                 </div>
                             </div>
-                            <div class="user-result-contact col-md-3 m-0">
+                            <div class="user-result-contact col-md-3 col-6 m-0">
                                 <h5>Agent Details</h5>
                                 <div class="contact-row">
                                     <i class="far fa-envelope"></i>
-                                    <p>: ` + resultSet[i].email + `</p>
+                                    <p>` + resultSet[i].email + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <i class="fas fa-phone-alt"></i>
-                                    <p>: ` + resultSet[i].phone + `</p>
+                                    <p>` + resultSet[i].phone + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <p><b style="font-weight: 600;">Member Since</b></p>
-                                    <p>: ` + dateMonthAsWord + `</p>
+                                    <p>` + dateMonthAsWord + `</p>
                                 </div>
                             </div>
                             </div>
@@ -153,9 +153,9 @@ function loadData(rs,loadObj) {
                 break;
                 case 2:
                     html = `<div class="col-md-12" data-aos="fade-up" data-aos-once="true">
-                            <div class="col-md-12 user-result" onClick="window.location='/user/` + resultSet[i]._id + `'">
-                                <div class="row col-md-9 m-0" style="padding: 10px;">
-                                <img src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
+                            <div class="col-md-12 user-result user-detail" onClick="window.location='/user/` + resultSet[i]._id + `'">
+                            <div class="row col-md-9 col-6 m-0" style="padding: 10px;">
+                                <img class="img-fluid" src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
                                 <div class="caption-result">
                                     <h4>`+ resultSet[i].firstName + ` ` + resultSet[i].lastName + `</h4>
                                     <h6>CEA: ` + resultSet[i].cea + `</h6>
@@ -170,19 +170,19 @@ function loadData(rs,loadObj) {
                                     <span class="fa fa-star"></span>
                                 </div>
                             </div>
-                            <div class="user-result-contact col-md-3 m-0">
+                            <div class="user-result-contact col-md-3 col-6 m-0">
                                 <h5>Agent Details</h5>
                                 <div class="contact-row">
                                     <i class="far fa-envelope"></i>
-                                    <p>: ` + resultSet[i].email + `</p>
+                                    <p>` + resultSet[i].email + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <i class="fas fa-phone-alt"></i>
-                                    <p>: ` + resultSet[i].phone + `</p>
+                                    <p>` + resultSet[i].phone + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <p><b style="font-weight: 600;">Member Since</b></p>
-                                    <p>: ` + dateMonthAsWord + `</p>
+                                    <p>` + dateMonthAsWord + `</p>
                                 </div>
                             </div>
                             </div>
@@ -190,9 +190,9 @@ function loadData(rs,loadObj) {
                 break;
                 case 3:
                     html = `<div class="col-md-12" data-aos="fade-up" data-aos-once="true">
-                            <div class="col-md-12 user-result" onClick="window.location='/user/` + resultSet[i]._id + `'">
-                                <div class="row col-md-9 m-0" style="padding: 10px;">
-                                <img src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
+                            <div class="col-md-12 user-result user-detail" onClick="window.location='/user/` + resultSet[i]._id + `'">
+                                <div class="row col-md-9 col-6 m-0" style="padding: 10px;">
+                                <img class="img-fluid" src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
                                 <div class="caption-result">
                                     <h4>`+ resultSet[i].firstName + ` ` + resultSet[i].lastName + `</h4>
                                     <h6>CEA: ` + resultSet[i].cea + `</h6>
@@ -207,19 +207,19 @@ function loadData(rs,loadObj) {
                                     <span class="fa fa-star"></span>
                                 </div>
                             </div>
-                            <div class="user-result-contact col-md-3 m-0">
+                            <div class="user-result-contact col-md-3 col-6 m-0">
                                 <h5>Agent Details</h5>
                                 <div class="contact-row">
                                     <i class="far fa-envelope"></i>
-                                    <p>: ` + resultSet[i].email + `</p>
+                                    <p>` + resultSet[i].email + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <i class="fas fa-phone-alt"></i>
-                                    <p>: ` + resultSet[i].phone + `</p>
+                                    <p>` + resultSet[i].phone + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <p><b style="font-weight: 600;">Member Since</b></p>
-                                    <p>: ` + dateMonthAsWord + `</p>
+                                    <p>` + dateMonthAsWord + `</p>
                                 </div>
                             </div>
                             </div>
@@ -228,8 +228,8 @@ function loadData(rs,loadObj) {
                 case 4:
                     html = `<div class="col-md-12" data-aos="fade-up" data-aos-once="true">
                             <div class="col-md-12 user-result" onClick="window.location='/user/` + resultSet[i]._id + `'">
-                                <div class="row col-md-9 m-0" style="padding: 10px;">
-                                <img src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
+                                <div class="row col-md-9 col-6 m-0 user-detail" style="padding: 10px;">
+                                <img class="img-fluid" src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
                                 <div class="caption-result">
                                     <h4>`+ resultSet[i].firstName + ` ` + resultSet[i].lastName + `</h4>
                                     <h6>CEA: ` + resultSet[i].cea + `</h6>
@@ -244,29 +244,32 @@ function loadData(rs,loadObj) {
                                     <span class="fa fa-star checked"></span>
                                 </div>
                             </div>
-                            <div class="user-result-contact col-md-3 m-0">
-                                <h5>Agent Details</h5>
-                                <div class="contact-row">
-                                    <i class="far fa-envelope"></i>
-                                    <p>: ` + resultSet[i].email + `</p>
-                                </div>
-                                <div class="contact-row">
-                                    <i class="fas fa-phone-alt"></i>
-                                    <p>: ` + resultSet[i].phone + `</p>
-                                </div>
-                                <div class="contact-row">
-                                    <p><b style="font-weight: 600;">Member Since</b></p>
-                                    <p>: ` + dateMonthAsWord + `</p>
-                                </div>
+                            <div class="user-result-contact col-md-3 col-6 m-0">
+                            <div class="col-md-12">
+                            <h5>Agent Details</h5>
+                            <div class="contact-row">
+                                <i class="far fa-envelope"></i>
+                                <p>` + resultSet[i].email + `</p>
+                            </div>
+                            <div class="contact-row">
+                                <i class="fas fa-phone-alt"></i>
+                                <p>` + resultSet[i].phone + `</p>
+                            </div>
+                            <div class="contact-row">
+                                <p><b style="font-weight: 600;">Member Since</b></p>
+                                <p>` + dateMonthAsWord + `</p>
+                            </div>
+                            </div>
+
                             </div>
                             </div>
                         </div>`
                 break;
                 default:
                     html = `<div class="col-md-12" data-aos="fade-up" data-aos-once="true">
-                            <div class="col-md-12 user-result" onClick="window.location='/user/` + resultSet[i]._id + `'">
-                                <div class="row col-md-9 m-0" style="padding: 10px;">
-                                <img src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
+                            <div class="col-md-12 user-result " onClick="window.location='/user/` + resultSet[i]._id + `'">
+                                <div class="row col-md-9 col-6 m-0 user-detail" style="padding: 10px;">
+                                <img class="img-fluid" src="` + resultSet[i].image + `" style="width : 200px; height: 200px;">
                                 <div class="caption-result">
                                     <h4>`+ resultSet[i].firstName + ` ` + resultSet[i].lastName + `</h4>
                                     <h6>CEA: ` + resultSet[i].cea + `</h6>
@@ -281,19 +284,19 @@ function loadData(rs,loadObj) {
                                     <span class="fa fa-star"></span>
                                 </div>
                             </div>
-                            <div class="user-result-contact col-md-3 m-0">
+                            <div class="user-result-contact col-md-3 col-6 m-0">
                                 <h5>Agent Details</h5>
                                 <div class="contact-row">
                                     <i class="far fa-envelope"></i>
-                                    <p>: ` + resultSet[i].email + `</p>
+                                    <p>` + resultSet[i].email + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <i class="fas fa-phone-alt"></i>
-                                    <p>: ` + resultSet[i].phone + `</p>
+                                    <p>` + resultSet[i].phone + `</p>
                                 </div>
                                 <div class="contact-row">
                                     <p><b style="font-weight: 600;">Member Since</b></p>
-                                    <p>: ` + dateMonthAsWord + `</p>
+                                    <p>` + dateMonthAsWord + `</p>
                                 </div>
                             </div>
                             </div>
