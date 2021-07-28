2ca321b30c75150e578b9ad959249a0fa3b5fe6f
dereckSOC <dereckzorca.18@ichat.sp.edu.sg>
2021-07-28 21:36:24 +0800

    new index.ejs

--- public/javascripts/propertySearch.js
+++ public/javascripts/propertySearch.js
@@ -422,105 +422,110 @@ function createHTMLListing(rs) {
 
     var htmlListing = `<div class="col-lg-12 col-xl-6 px-0" data-aos="fade-up" data-aos-once="true">
                     <div class="listingHoverIndex pt-3" style="display: flex; align-items: center; justify-content: center; flex-direction: column; margin: 10px; transition: 0.3s; " >
-                        <div class="row col-md-12">
+                        <div class="row col-md-12 m-0 p-0">
 
-                            ` + insertSoldArchive + `
-
-                            <div class="row col-md-12 m-0 d-flex justify-content-between align-items-center" style="padding: 0 0 10px 0;">
-                                <div class="d-flex flex-direction-row align-items-center justify-content-center" style="font-size: 15px; cursor: pointer; " onClick=" window.location='/user/` + author_id + `'">
-                                    <img src="` + auther_id_img + `" style="width:35px; height:35px ;border-radius: 50%; margin: auto;object-fit: cover;">
-                                    <div class="d-flex flex-column" style="margin-left: 10px;">
-                                        <div class="d-flex flex-direction-row">
-                                            ` + first_name + `
-                                            ` + last_name + `
-                                        </div>
-                                        <div style="font-size: 12px;">
-                                            ` + date + `
-                                        </div>
-                                    </div>
-                                </div>
-                
-                                <div class="d-flex flex-direction-row">
-                                    <div class="d-flex align-items-center justify-content-end">
-                                        <form action="/listings/` + id + `/like" method="POST" class="likeBtn">
-                                            <div>
-                                                ` + insertLikeUnlike + `
-                                            </div>
-                                        </form>
-                                    </div>
-
-                                    ` + insertManageListStart + `
-                                    ` + listArchiveSold + `
-                                    ` + listUnArchive + `
-                                    ` + listMarkAvail + `
-                                    ` + insertManageListEnd + `
-                                    
-        
-                                </div>
-                            </div>
+                        ` + insertSoldArchive + `
 
                             <!--Left of Listing-->
                             <div class="col-sm-4 col-md-4 p-0 m-0 list-left d-flex justify-content-center align-items-center ">
                                 <a class="p-0 m-0" href="/listings/` + id + `" style="text-decoration: none; color: black">
-                                    <img class="img-fluid shadow" src="` + thumbnail +`" style="width: 100%; height: 140px; object-fit: cover; border-radius: 5px;">
+                                    <img class="img-fluid shadow" src="` + thumbnail +`" style="width: 100%; height: 200px; object-fit: cover;">
                                 </a>
                             </div>
 
                             <!--Right of Listing-->
                             <div class="col-sm-8 col-md-8 list-right" > 
-                                <a href="/listings/` + id + `" style="text-decoration: none; color: black;">       
-                                    <!--Name-->
-                                    <div class="col-12 listing-detail d-flex justify-content-between" id="listing-name" >
 
-                                        ` + listing_name + `
+                                <div class="col-md-12 m-0 p-0">
+                                        <!--Name-->
+                                        <div class="row col-12 m-0 d-flex flex-direction-row align-items-center justify-content-between" style="padding-left: 15px; padding-right: 15px;">
+                                            <div onClick='window.location="/listings/` + id + `"' class="listing-detail d-flex justify-content-between" id="listing-name" style="cursor:pointer">
 
+                                                ` + listing_name + `
+
+                                            </div>
+
+                                            <div class="d-flex justify-content-center align-items-center">
+                                                <div class="d-flex flex-direction-row">
+                                                    <div class="d-flex align-items-center justify-content-end">
+                                                        <form action="/listings/` + id + `/like" method="POST" class="likeBtn">
+                                                            <div>
+                                                                ` + insertLikeUnlike + `
+                                                            </div>
+                                                        </form>
+                                                    </div>
+            
+                                                    ` + insertManageListStart + `
+                                                    ` + listArchiveSold + `
+                                                    ` + listUnArchive + `
+                                                    ` + listMarkAvail + `
+                                                    ` + insertManageListEnd + `
+                                                
+                    
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!--Price-->
+                                        <div class="col-12 listing-detail mt-1" id="listing-price"  onClick='window.location="/listings/` + id + `"' style="cursor:pointer">
+                                            <p style="margin-bottom: 0">S$ </p>
+
+                                            <span>` + price + `</span>
+                                            
+                                        </div>
+                                        <!--Property Details-->
+                                        <div class="col-12 align-items-center d-flex mt-2"  onClick='window.location="/listings/` + id + `"' style="cursor:pointer">
+                                            <div class="left">
+                                                <span>`+ bedrooms +`</span>
+                                                <img class="pl-1" src="/img/bed-solid-svgrepo-com.svg" alt="" style="width:26px!important;">
+                                            </div>
+
+                                            <div class="left right">
+                                                <span>`+ bathrooms +`</span>
+                                                <img class="pl-1" src="/img/wc-toilet-svgrepo-com.svg" alt="" style="width:20px!important;">
+                                            </div>
+
+                                            <div class="left right">
+                                                <i style="font-size:0.26em!important;" class="fas fa-circle"></i>
+                                            </div>
+
+                                            <div class="right">
+                                                <span>`+ size +` sqft</span>
+                                            </div>
+                                        </div>
+                                        <!--Tags-->
+                                        <div class="col-12 align-items-center d-flex"  onClick='window.location="/listings/` + id + `"' style="cursor:pointer"> 
+                                            <div class="left pt-2 pb-1">
+                                                <span class="px-3 py-0" style="color: #4632DA; border: 1px solid #4632DA;">
+                                                        `+ district +`
+                                                </span>
+                                            </div>
+                                            <div class="left right pt-2 pb-1">
+                                                <span class="px-3 py-0" style="color: #4632DA; border: 1px solid #4632DA;">
+                                                        `+ type +`
+                                                </span>
+                                            </div>
+                                            <div class="left right pt-2 pb-1">
+                                                <span class="px-3 py-0" style="color: #4632DA; border: 1px solid #4632DA;">
+                                                        `+ tenure +`
+                                                </span>
+                                            </div>
+                                        </div>
+                                </div>
+                                <div class="col-md-12 m-0 p-0">
+                                    <div class="d-flex flex-direction-row align-items-center justify-content-start" style="font-size: 15px; cursor: pointer;padding-left: 15px; padding-top: 10px; " onClick=" window.location='/user/` + author_id + `'">
+                                        <img src="` + auther_id_img + `" style="width:35px; height:35px ;border-radius: 50%; margin: 0;object-fit: cover; ">
+                                        <div class="d-flex flex-column" style="margin-left: 10px;">
+                                            <div class="d-flex flex-direction-row">
+                                                ` + first_name + `
+                                                ` + last_name + `
+                                            </div>
+                                            <div style="font-size: 12px;">
+                                                ` + date + `
+                                            </div>
+                                        </div>
                                     </div>
-                                    <!--Price-->
-                                    <div class="col-12 listing-detail mt-1" id="listing-price">
-                                        <p style="margin-bottom: 0">S$ </p>
-
-                                        <span>` + price + `</span>
-                                        
-                                    </div>
-                                    <!--Property Details-->
-                                    <div class="col-md-12 align-items-center d-flex mt-2">
-                                        <div class="left">
-                                            <span>`+ bedrooms +`</span>
-                                            <img class="pl-1" src="/img/bed-solid-svgrepo-com.svg" alt="" style="width:26px!important;">
-                                        </div>
-
-                                        <div class="left right">
-                                            <span>`+ bathrooms +`</span>
-                                            <img class="pl-1" src="/img/wc-toilet-svgrepo-com.svg" alt="" style="width:20px!important;">
-                                        </div>
-
-                                        <div class="left right">
-                                            <i style="font-size:0.26em!important;" class="fas fa-circle"></i>
-                                        </div>
-
-                                        <div class="right">
-                                            <span>`+ size +` sqft</span>
-                                        </div>
-                                    </div>
-                                    <!--Tags-->
-                                    <div class="col-md-12"> 
-                                        <div class="left pt-2 pb-1">
-                                            <span class="px-3 py-0" style="color: #4632DA; border: 1px solid #4632DA;">
-                                                    `+ district +`
-                                            </span>
-                                        </div>
-                                        <div class="left right pt-2 pb-1">
-                                            <span class="px-3 py-0" style="color: #4632DA; border: 1px solid #4632DA;">
-                                                    `+ type +`
-                                            </span>
-                                        </div>
-                                        <div class="left right pt-2 pb-1">
-                                            <span class="px-3 py-0" style="color: #4632DA; border: 1px solid #4632DA;">
-                                                    `+ tenure +`
-                                            </span>
-                                        </div>
-                                    </div>
-                                </a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -528,75 +533,50 @@ function createHTMLListing(rs) {
     return htmlListing;
 }
 
-function loadDataListing (rs,loadObj) {
-    //loadObj
-    var totalRem = loadObj.totalRem;
-    var sectionRem = loadObj.sectionRem;
-    var remainList = loadObj.remainList;
-    var startIndex = loadObj.startIndex;
-    var endIndex;
-
-    //loads all data
-    var insertDiv = document.getElementById("loadProperty");
-    var i;
-
-    if (sectionRem == 0) {
-        endIndex = startIndex + remainList ;
-        //remaining minus 5 (as it has already been loaded)
-        loadObj.totalRem = 0;
-        //section minus 1 (as it has already been loaded)
-        loadObj.sectionRem = 0;
-        //remainList remains the same
-        loadObj.remainList = 0;
-        //end index becomes start index
-        loadObj.startIndex = 0;
-    } else {
-        endIndex = startIndex + 10;
-    }
-
-    for (i = startIndex; i < endIndex; i++) {
-        var htmlListing = createHTMLListing(rs[i]);
-        insertDiv.insertAdjacentHTML("beforeend", htmlListing);
-    }
-
-     //update object
-    //if section is not done
-    if (sectionRem != 0) {
-        loadObj.totalRem = loadObj.totalRem - 10;
-        loadObj.sectionRem = loadObj.sectionRem - 1;
-        loadObj.startIndex = endIndex;
-    } else {
-        loadObj.totalRem = loadObj.remainList;
-        loadObj.sectionRem = 0;
-        loadObj.startIndex = endIndex;
-        document.getElementById("loadMoreBtn").remove();
-        var finalMsg = `<p id="endOfResult">End of Result</p>`
-        try {
-            document.getElementById("loadMoreDiv").insertAdjacentHTML("beforeend", finalMsg);
-        } catch(e){}
-        
-    }
-
-    return loadObj;
-}
-
 function loadFirstFiveListing(rs) {
-    var resultSet = rs;
-    var rsLength = resultSet.length;
-    var loadObj;
+   //take top 4 most likes 
+   var likeHolderArr = [0,0,0,0];
+   var listHolderArr = [0,0,0,0];
+   for (var e = 0; e < rs.length; e++) {
+       if (rs[e].likes.length > likeHolderArr[0]) {
+           likeHolderArr[0] = rs[e].likes.length;
+           listHolderArr[0] = rs[e];
+       }
+       else if (rs[e].likes.length > likeHolderArr[1]) {
+           likeHolderArr[1] = rs[e].likes.length;
+           listHolderArr[1] = rs[e];
+       }
+       else if (rs[e].likes.length > likeHolderArr[2]) {
+           likeHolderArr[2] = rs[e].likes.length;
+           listHolderArr[2] = rs[e];
+       }
+       else if (rs[e].likes.length > likeHolderArr[3]) {
+           likeHolderArr[3] = rs[e].likes.length;
+           listHolderArr[3] = rs[e];
+       }
+   }
 
-    var section = Math.floor(parseInt(rsLength, 10) / 10); //number of sections (1 section, 10 listings)
-    // var remSect = parseInt(rsLength,10) % 5; //number of listings remaining
-    var remSect = rsLength - (section * 10);
+   var insertDiv = document.getElementById("loadProperty");
 
-    var loadObj = {
-        "totalRem" : rsLength,
-        "sectionRem" : section,
-        "remainList" : remSect,
-        "startIndex" : 0,
-    }
-
-
-    loadObj = loadDataListing(rs,loadObj);
-    return loadObj;
+   for (var i = 0; i < likeHolderArr.length; i++) {
+       var htmlListing = createHTMLListing(listHolderArr[i]);
+       insertDiv.insertAdjacentHTML("beforeend", htmlListing);
+   };
 }
+
+function loadFeatured(rs) {
+    //take 4 listings
+    var listHolderArr = [0,0,0,0];
+    for (var e = 0; e < 4; e++) {
+       listHolderArr[e] = rs[e]
+    }
+ 
+    console.log(listHolderArr)
+
+    var insertDiv = document.getElementById("loadFeaturedProperty");
+ 
+    for (var i = 0; i < listHolderArr.length; i++) {
+        var htmlListing = createHTMLListing(listHolderArr[i]);
+        insertDiv.insertAdjacentHTML("beforeend", htmlListing);
+    };
+ }
