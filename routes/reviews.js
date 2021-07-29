ab74373462ffd1153a459b5d6b65f3c08a6fbcf3
yapzongxin <YAPZX.19@ichat.sp.edu.sg>
2021-07-29 20:31:49 +0800

    seeker profile

--- routes/reviews.js
+++ routes/reviews.js
@@ -31,6 +31,17 @@ router.get("/user/:id/reviews/new", middleware.isLoggedIn, middleware.checkRevie
         res.render("reviews/new.ejs", {user: user});
     });
 });
+// Reviews New Seeker
+router.get("/user/:id/reviews/new/seeker", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
+    // middleware.checkReviewExistence checks if a user already reviewed the user, only one review per user is allowed
+    User.findById(req.params.id, function (err, user) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        res.render("reviews/newSeeker.ejs", {user: user});
+    });
+});
 
 // Reviews Create
 router.post("/user/:id/reviews", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
@@ -59,6 +70,36 @@ router.post("/user/:id/reviews", middleware.isLoggedIn, middleware.checkReviewEx
             user.save();
             req.flash("success", "You have successfully added a review.");
             res.redirect('/user/' + user._id + '/reviews');
+        });
+    });
+});
+// Reviews Create Seeker
+router.post("/user/:id/reviews/seeker", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
+    //lookup user using ID
+    User.findById(req.params.id).populate("reviews").exec(function (err, user) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        Review.create(req.body.review, function (err, review) {
+            if (err) {
+                req.flash("error", err.message);
+                return res.redirect("back");
+            }
+            //add author username/id and associated user to the review
+            review.author.id = req.user._id;
+            review.author.username = req.user.username;
+            review.author.image = req.user.image;
+            review.user = user;
+            //save review
+            review.save();
+            user.reviews.push(review);
+            // calculate the new average review for the user
+            user.rating = calculateAverage(user.reviews);
+            //save user
+            user.save();
+            req.flash("success", "You have successfully added a review.");
+            res.redirect('/user/' + user._id);
         });
     });
 });
@@ -92,6 +133,37 @@ router.get("/user/:id/reviews/:review_id/edit", middleware.checkReviewOwnership,
             return res.redirect("back"); 
         }   
         res.render("reviews/edit.ejs", {user_id: req.params.id, review: foundReview});
+    });
+});
+// Reviews Edit Seeker
+router.get("/user/:id/reviews/:review_id/edit/seeker", middleware.checkReviewOwnership, function (req, res) {
+    Review.findById(req.params.review_id, function (err, foundReview) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        var reviewDate; 
+        if(foundReview.updatedAt) {
+            reviewDate = foundReview.updatedAt;
+            console.log('have been updated: '+ reviewDate);
+        }else {
+            reviewDate = foundReview.createdAt;
+            console.log('original not updated: '+reviewDate);
+        }
+        var latestReview = moment(reviewDate);
+        var expireReview = latestReview.clone().add(1, 'weeks');  //.add(10, 'seconds');
+        console.log('latest foundReview: '+latestReview);
+        console.log('expire Reivew: '+expireReview);
+
+        var currentDate = moment();
+        console.log('current date now: '+currentDate);
+        if (currentDate >= expireReview){
+            console.log('edit button should be expired')
+            // clearInterval(intervalCheck);
+            req.flash("error", 'You are no longer allowed to edit the review.');
+            return res.redirect("back"); 
+        }   
+        res.render("reviews/editSeeker.ejs", {user_id: req.params.id, review: foundReview});
     });
 });
 // Reviews Edit at Dashboard
@@ -147,6 +219,27 @@ router.put("/user/:id/reviews/:review_id", middleware.checkReviewOwnership, func
         });
     });
 });
+// Reviews Update Seeker
+router.put("/user/:id/reviews/:review_id/seeker", middleware.checkReviewOwnership, function (req, res) {
+    Review.findByIdAndUpdate(req.params.review_id, req.body.review, {new: true}, function (err, updatedReview) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        User.findById(req.params.id).populate("reviews").exec(function (err, user) {
+            if (err) {
+                req.flash("error", err.message);
+                return res.redirect("back");
+            }
+            // recalculate user average
+            user.rating = calculateAverage(user.reviews);
+            //save changes
+            user.save();
+            req.flash("success", "You have successfully edited a review.");
+            res.redirect('/user/' + user._id);
+        });
+    });
+});
 // Reviews Update at Dashboard
 router.put("/user/:id/reviews/:review_id/dashboard", middleware.checkReviewOwnership, function (req, res) {
     Review.findByIdAndUpdate(req.params.review_id, req.body.review, {new: true}, function (err, updatedReview) {
@@ -187,6 +280,27 @@ router.delete("/user/:id/reviews/:review_id", middleware.checkReviewOwnership, f
             user.save();
             req.flash("success", "You have successfully deleted a review.");
             res.redirect("/user/" + req.params.id + '/reviews');
+        });
+    });
+});
+// Reviews Delete Seeker
+router.delete("/user/:id/reviews/:review_id/seeker", middleware.checkReviewOwnership, function (req, res) {
+    Review.findByIdAndRemove(req.params.review_id, function (err) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        User.findByIdAndUpdate(req.params.id, {$pull: {reviews: req.params.review_id}}, {new: true}).populate("reviews").exec(function (err, user) {
+            if (err) {
+                req.flash("error", err.message);
+                return res.redirect("back");
+            }
+            // recalculate user average
+            user.rating = calculateAverage(user.reviews);
+            //save changes
+            user.save();
+            req.flash("success", "You have successfully deleted a review.");
+            res.redirect("/user/" + req.params.id);
         });
     });
 });
