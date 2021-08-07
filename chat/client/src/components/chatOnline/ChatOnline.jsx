2f4becc26823f2b1d6f317e8366a2b01f92b29b8
avke03 <avk03.19@ichat.sp.edu.sg>
2021-08-07 17:21:44 +0800

    chat fix 13

--- chat/client/src/components/chatOnline/ChatOnline.jsx
+++ chat/client/src/components/chatOnline/ChatOnline.jsx
@@ -203,7 +203,7 @@ export default function ChatOnline({ currentId, conv }) {
       {user ? (
         <>
           <div>
-            {user &&
+            {(user !==null) &&
               <div className="userImg">
                 <img
                   className="chatOnlineImg"
